a72374a94aacc802857a44b5fa1b803a55611762
himanmen Git <study.himanmen@gmail.com>
2018-02-05 15:14:30 +0900

    foreignkey field에서 primary_key를 따로 지정한 경우 실습

--- django/foreignkey/models.py
+++ django/foreignkey/models.py
@@ -1,16 +1,9 @@
 from django.db import models
 
 
-class Manufacturer(models.Model):
-    name = models.CharField('제조사명', max_length=50)
-
-    def __str__(self):
-        return self.name
-
-
 class Car(models.Model):
     manufacturer = models.ForeignKey(
-        Manufacturer,
+        'Manufacturer',
         on_delete=models.CASCADE,
         verbose_name='제조사',
     )
@@ -21,3 +14,39 @@ class Car(models.Model):
             manufacturer=self.manufacturer.name,
             car_name=self.name
         )
+
+
+class Manufacturer(models.Model):
+    name = models.CharField('제조사명', max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=60)
+    teacher = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Type(models.Model):
+    name = models.CharField(primary_key=True, max_length=60)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Pokemon(models.Model):
+    dex_number = models.IntegerField(primary_key=True)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.dex_number:03}. {self.name} ({self.type.name})'
