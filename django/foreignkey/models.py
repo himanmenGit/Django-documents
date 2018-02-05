0c439119a14e29d481d2f0b74173ba52326f6b59
himanmen Git <study.himanmen@gmail.com>
2018-02-05 14:34:17 +0900

    foreignkey app추가, Manufacturer/Car모델 정의

---
+++ django/foreignkey/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Manufacturer(models.Model):
+    name = models.CharField('제조사명', max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        on_delete=models.CASCADE,
+        verbose_name='제조사',
+    )
+    name = models.CharField('모델명', max_length=60)
+
+    def __str__(self):
+        return '{manufacturer} {car_name}'.format(
+            manufacturer=self.manufacturer.name,
+            car_name=self.name
+        )
