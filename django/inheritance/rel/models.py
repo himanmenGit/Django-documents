c29f8f823a1a52ee0710857a2e90b78a503415ba
himanmen Git <study.himanmen@gmail.com>
2018-02-08 16:56:18 +0900

    rPerson, Item[Fruit, Food] 구성

--- django/inheritance/rel/models.py
+++ django/inheritance/rel/models.py
@@ -1,2 +1,35 @@
 from django.db import models
 
+
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    person = models.ForeignKey(
+        Person,
+        on_delete=models.CASCADE,
+        related_name='%(app_label)s_%(class)ss',
+        related_query_name='%(app_label)s_%(class)s',
+        # related_query_name='item',
+    )
+
+    class Meta:
+        abstract = True
+
+
+class Fruit(Item):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Food(Item):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
